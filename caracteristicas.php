0d2a74eaea454f35aad41803df98ef43f7bbe964
valena <lvalegria.cds@gmail.com>
2022-05-30 16:47:31 -0500

    paginas php

---
+++ caracteristicas.php
@@ -0,0 +1,5 @@
+<?php include "head.php"?>
+
+<h1>Caracteristicas</h1>
+
+<?php include "footer.php"?>
---
+++ contacto.php
@@ -0,0 +1,5 @@
+<?php include "head.php"?>
+
+<h1>Contacto</h1>
+
+<?php include "footer.php"?>
