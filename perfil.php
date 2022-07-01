9c888a864f7a6ceb8e25f0bef7d1bbadc6913e92
lvalegria <lvalegria.cds@gmail.com>
2022-06-30 20:24:48 -0500

    primera

--- perfil.php
+++ perfil.php
@@ -1,6 +1,20 @@
 <?php include "head.php"?>
 
 <h1>perfil</h1>
+<div class="container">
+    <div class="row pokemonPerfil mt-4">
+        <div class="col-md-4 imgContentPok">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png" alt="" srcset="" id="imgPok" class="mx-auto d-block">
+
+        </div>
+
+        <div class="col-md-8">
+
+        </div>
+
+    </div>
+
+</div>
 
 <script src="js/perfil.js"></script>
 
